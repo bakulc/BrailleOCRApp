5a2b0ae979f963221811b62c75d35bf9119bdd1b
Bakul Choudhary <choudharybakul809kv@gmail.com>
2019-01-29 00:01:17 +0530

    Refine the Layouts and Add intent to move to test activity

--- app/src/main/java/com/example/brailleocrapp/MainActivity.java
+++ app/src/main/java/com/example/brailleocrapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.brailleocrapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(this, CustomTextSend.class);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+        button.setText("Test App");
     }
 }
