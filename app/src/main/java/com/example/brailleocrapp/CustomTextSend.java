babfcf308368bef906fedfc2bc4ab135e56ab1af
Bakul Choudhary <choudharybakul809kv@gmail.com>
2019-01-29 23:22:52 +0530

    Add Visual elements to Custom Text and clear function. Delete v26/layouts. (Safe Commit)

--- app/src/main/java/com/example/brailleocrapp/CustomTextSend.java
+++ app/src/main/java/com/example/brailleocrapp/CustomTextSend.java
@@ -2,12 +2,48 @@ package com.example.brailleocrapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CustomTextSend extends AppCompatActivity {
 
+
+    Button sendButton, clearButton;
+    EditText enterText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_text_send);
+
+        sendButton  = findViewById(R.id.sendButton);
+        clearButton = findViewById(R.id.clearButton);
+
+
+
+        enterText = findViewById(R.id.enterText);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessageWiFi();
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearText(enterText);
+            }
+        });
+    }
+
+    public void sendMessageWiFi() {
+        String message = enterText.getText().toString();
+
+    }
+
+    public void clearText(EditText editText) {
+        editText.setText("");
     }
 }
